1c86998ea53fcdd0d28068c173692436b047d9e2
Your Name <you@example.com>
2022-03-01 12:07:12 +0200

    first commit

---
+++ pyghaseel/__init__.py
@@ -0,0 +1,3 @@
+from .Cleaner import Cleaner
+from .ar_cleaner import Ar_cleaner
+from .en_cleaner import En_cleaner
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='pyghaseel',
+      version='0.1',
+      description='Simple text cleaning package that supports Arabic and English text',
+      install_requires=[
+        'regex',
+        'numpy',
+        'pandas',
+        'mysql-connector-python',
+        'nltk',
+        'tashaphyne',
+        'qalsadi',
+        'Arabic-Stopwords',
+        'pyarabic'
+        ],
+      packages=['pyghaseel'],
+      zip_safe=False)
